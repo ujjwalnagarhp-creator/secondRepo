233079e2dcea3bd70860ae0c2747367ca3aa454e
Ujjwal <ujjwalnagarhp@gmail.com>
2026-01-06 21:13:29 +0530

    Assignment

---
+++ Assingment1/1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int checkArr(int nArr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(nArr[i]==nArr[j]){return 1;}
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int n;scanf("%d",&n);
+    if(n<=0){
+        printf("Enter positive value of n");
+        return 0;
+    }
+    
+    int nArr[n];
+    
+    for(int i=0;i<n;i++){
+        if(!scanf("%d",&nArr[i])){
+            printf("Enter valid values!!");
+            return 0;
+        }
+    }
+
+    if(checkArr(nArr,n)){printf("Values are not Unique!!");return 0;}
+    
+    printf("Enter value of k: ");
+    int k;scanf("%d",&k);
+
+
+    int flag=1;
+    for(int i=0;i<n;i++){
+        if(nArr[i]==k){
+            printf("%d ",i);
+            flag=0;
+        }
+    }
+    if(flag){printf("-1");}
+    
+}
